9c3d200af20456358a326c6d169a71aa4c6a0cb7
Alessandro Vullo <avullo@ebi.ac.uk>
2015-02-23 12:48:03 +0000

    Skeleton for the conversion to 1.0 JSON format specification. The translation routine accepts the trackhub object and the assembly as parameters. A yet to be implemented routine is in charge of fetching detailed assembly/species information.

--- src/Registry/lib/Registry/TrackHub/Translator.pm
+++ src/Registry/lib/Registry/TrackHub/Translator.pm
@@ -11,6 +11,7 @@ use JSON;
 use Catalyst::Exception;
 
 use Registry::TrackHub;
+use Registry::TrackHub::Parser;
 
 use vars qw($AUTOLOAD);
 
@@ -38,12 +39,16 @@ sub new {
   return $self;
 }
 
+#
+# TODO
+# validate JSON docs, use Registry->config()->{TrackHub}{json}{schema};
+#
 sub translate {
   my ($self, $url, $assembly) = @_;
 
   my $dispatch = 
     {
-     1 => sub { $self->to_json_1(@_) }
+     '1.0' => sub { $self->to_json_1_0(@_) }
     }->{$self->version};
 
   Catalyst::Exception->throw(sprintf "Version %d not supported", $self->version) 
@@ -52,31 +57,55 @@ sub translate {
   my $trackhub = Registry::TrackHub->new(url => $url);
   
   my $docs;
-  my $parser;
-  unless ($assembly) { # translate tracksDB for all assemblies stored in the Hub
+  unless ($assembly) { 
+    # assembly not specified
+    # translate tracksDB conf for all assemblies stored in the Hub
     foreach my $assembly ($trackhub->assemblies) {
-      $parser = Registry::TrackHub::Parser->new(files => $trackhub->trackdb_conf_for_assembly($assembly));
       push @{$docs}, $dispatch->(trackhub => $trackhub, 
-				 tracks   => $parser->parse);
+				 assembly => $assembly);
     }
   } else {
-    $parser = Registry::TrackHub::Parser->new(files => $trackhub->trackdb_conf_for_assembly($assembly));
     push @{$docs}, $dispatch->(trackhub => $trackhub, 
-			       tracks   => $parser->parse);
+			       assembly => $assembly);
   }
 
   scalar @{$docs} or 
     Catalyst::Exception->throw("Something went wrong. Couldn't get any translated JSON from hub");
+
   return $docs;
 }
 
-sub to_json_1 {
+sub to_json_1_0 {
   my ($self, %args) = @_;
-  my ($trackhub, $tracks) = ($args{trackhub}, $args{tracks});
-  defined $trackhub and defined $tracks or
-    Catalyst::Exception->throw("Undefined trackhub and/or tracks argument");
+  my ($trackhub, $assembly) = ($args{trackhub}, $args{assembly});
+  defined $trackhub and defined $assembly or
+    Catalyst::Exception->throw("Undefined trackhub and/or assembly argument");
 
-  return to_json({ test => 'test' });
+  my $genome = $trackhub->get_genome($assembly);
+
+  my $doc = 
+    {
+     version => '1.0',
+     hub     => $trackhub->longLabel,
+     # TODO
+     # add the original trackDb file
+    };
+
+  $self->_add_genome_info($genome, $doc);
+  my $tracks = Registry::TrackHub::Parser->new(files => $genome->trackDb)->parse;
+
+  return to_json($doc);
+}
+
+#
+# Add species/assembly info
+#
+# I presume this can be shared across translations
+# to different versions
+#
+sub _add_genome_info {
+  my ($self, $genome, $doc) = @_;
+
 }
 
 1;

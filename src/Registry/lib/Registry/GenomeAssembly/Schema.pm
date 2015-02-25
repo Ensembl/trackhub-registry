fb1c4e0a4793a93540cd6d1a23db1b4006fcb059
Alessandro Vullo <avullo@ebi.ac.uk>
2015-02-25 12:42:55 +0000

    The DBIx::Class schema class to interface to the Genome Collection database (Oracle).

---
+++ src/Registry/lib/Registry/GenomeAssembly/Schema.pm
@@ -0,0 +1,20 @@
+use utf8;
+package Registry::GenomeAssembly::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-02-25 12:11:37
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:79ItXYNPzOfpFO7q1Y/UJQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;

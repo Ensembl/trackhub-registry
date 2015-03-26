e31215f505324ba2d5bcd74350812df056b5ce0b
Alessandro Vullo <avullo@ebi.ac.uk>
2015-03-26 11:32:40 +0000

    Controller to emit error responses. It is necessary since the API does not behave as expected in certain circumnstances. E.g. If an exception is raised when creating a trackhub doc, and it is catched inside a try/catch block, invoking status_bad_request will properly format the error message but the flow of control will continue up to the next status_created method invocation, returning an incorrect 201 status code and non-sense location parameter. Invoking the error handling action of a separate controller eliminates the problem, as the flow of control does not return to the API.

---
+++ src/Registry/lib/Registry/Controller/ReturnError.pm
@@ -0,0 +1,67 @@
+package Registry::Controller::ReturnError;
+use Moose;
+use namespace::autoclean;
+use Carp::Clan qw(^Registry::Controller::);
+BEGIN { extends 'Catalyst::Controller::REST'; }
+
+__PACKAGE__->config(
+    'default'   => 'application/json',
+#     'stash_key' => 'rest',
+    'map'       => {
+        'text/x-yaml'       => 'YAML::XS',
+        'application/json'  => 'JSON::XS',
+        'text/plain'        => 'JSON::XS',
+        'text/html'         => 'YAML::HTML',
+    }
+);
+
+sub index : Path : Args(0) : ActionClass('REST') {
+    my ( $self, $c, $raw_error ) = @_;
+
+    $c->log->error($raw_error);
+
+
+    #     #     $error =~ s/\n/\\x/g;
+    #     $error = "thist!";
+    #     $error =~ s/s/\n/g;
+    #     $c->log->warn( 'ERROR: ', $error );
+    # my $raw_msg = $c->stash->{error};
+    my ($error_cleaned) = $raw_error =~ m/MSG:\s(.*?)STACK/s;
+    $error_cleaned ||= 'something bad has happened';
+    $error_cleaned =~ s/\n//g;
+    $self->status_bad_request( $c, message => $error_cleaned );
+}
+
+sub index_GET { }
+sub index_POST { }
+
+sub custom : Path : Args(0) : ActionClass('REST') {
+  my ( $self, $c, $error_msg ) = @_;
+  $c->log->error($error_msg);
+    
+  $self->status_bad_request( $c, message => $error_msg );
+}
+
+sub custom_GET { }
+sub custom_POST { }
+
+sub no_content: Path : Args(0) : ActionClass('REST') {
+  my ( $self, $c, $error_msg ) = @_;
+  $c->log->error($error_msg);
+  $self->status_no_content( $c, message => $error_msg );
+}
+
+sub no_content_GET { }
+sub no_content_POST { }
+
+sub not_found: Path : Args(0) : ActionClass('REST') {
+  my ( $self, $c, $error_msg ) = @_;
+  $self->status_not_found($c, message => $error_msg);
+}
+
+sub not_found_GET { }
+sub not_found_POST { }
+
+__PACKAGE__->meta->make_immutable;
+
+1;

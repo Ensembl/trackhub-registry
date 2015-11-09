a4eab25cf6d94785aaa2cadc208a6edbe8c77383
Alessandro Vullo <avullo@ebi.ac.uk>
2015-11-09 12:52:43 +0000

    [ENSCORESW-1484]. An extra body argument to register publicly available hubs or not.

--- src/Registry/lib/Registry/Controller/API/Registration.pm
+++ src/Registry/lib/Registry/Controller/API/Registration.pm
@@ -269,6 +269,8 @@ sub trackhub_POST {
   my $url = $c->req->data->{url};
   my $trackdb_type = $c->req->data->{type} || 'genomics'; # default to genomics type
   my $assembly_map = $c->req->data->{assemblies}; # might have submitted name -> accession map in case of non-UCSC assemblies
+  my $public = # whether the trackDbs are available for search or not, default: yes
+    defined $c->req->data->{public}?$c->req->data->{public}:1; 
   
   return $self->status_bad_request($c, message => "You must specify the remote trackhub URL")
     unless $url;
@@ -355,6 +357,8 @@ sub trackhub_POST {
     foreach my $json_doc (@{$trackdbs_docs}) {
       my $doc = from_json($json_doc);
       
+      $doc->{public} = $public;
+
       # add type
       $doc->{type} = $trackdb_type;
 

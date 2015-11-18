cd577ebfb440099f46bde7cb12f39f1c218a1390
Alessandro Vullo <avullo@ebi.ac.uk>
2015-11-18 12:36:56 +0000

    Already included in examples.

--- docs/api/https_connect.py
+++
@@ -1,8 +0,0 @@
-import requests, sys
-
-# see http://docs.python-requests.org/en/latest/user/advanced/#ssl-cert-verification
-
-r = requests.get('https://twitter.com', verify=True)
-if not r.ok:
-    r.raise_for_status()
-    sys.exit()

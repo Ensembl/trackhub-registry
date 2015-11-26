a9d35ffa1ee45f51d02641004b6f29d09c0ce494
Alessandro Vullo <avullo@ebi.ac.uk>
2015-11-26 16:27:17 +0000

    [ENSCORESW-1572]. Changes to reflect latest updates.

--- src/Registry/script/production/update_trackdb_status.pl
+++ src/Registry/script/production/update_trackdb_status.pl
@@ -22,19 +22,23 @@ use JSON;
 use Data::Dumper;
 
 use HTTP::Tiny;
+use HTTP::Headers;
+use HTTP::Request::Common;
+use LWP::UserAgent;
+
 use Search::Elasticsearch;
 
 use Registry::Model::Search;
 use Registry::TrackHub::TrackDB;
-use Registry::TrackHub::Translator;
-use Registry::TrackHub::Validator;
+# use Registry::TrackHub::Translator;
+# use Registry::TrackHub::Validator;
 use Registry::Utils::Arrays qw( remove_duplicates union_intersection_difference );
 
 my $logger = get_logger();
 
 # default option values
 my $help = 0;
-my $log_dir = '.logs';
+my $log_dir = 'logs';
 my $conf_file = '.initrc'; # expect file in current directory
 
 # parse command-line arguments
@@ -70,14 +74,16 @@ LOGCONF
 
 Log::Log4perl->init(\$log_conf);
 
-$logger->info("Reading configuration file $config_file");
+$logger->info("Reading configuration file $conf_file");
 my %config;
 eval {
-  read_config $config_file => %config
+  read_config $conf_file => %config
 };
-$logger->logdie("Error reading configuration file $config_file: $@") if $@;
+$logger->logdie("Error reading configuration file $conf_file: $@") if $@;
 
 $logger->info("Checking the cluster is up and running");
+$logger->logdie("Couldn't find ES node configuration")
+  unless $config{cluster}{nodes};
 my $esurl;
 if (ref $config{cluster}{nodes} eq 'ARRAY') {
   $esurl = sprintf "http://%s", $config{cluster}{nodes}[0];
@@ -119,7 +125,7 @@ try {
   $users = $es->get_all_users;
   map { $_->{username} =~ /$config{users}{admin_name}/ and $admin = $_ } @{$users};
 } catch {
-  $logger->logdie($_);
+  $logger->logdie("Couldn't get latest report/user list:\n$_");
 };
 
 $admin or $logger->logdie("Unable to find admin user.");
@@ -138,7 +144,8 @@ my $current_report = {};
 #   notify
 foreach my $user (@{$users}) {
   my $username = $user->{username};
-  if ($username =~ /$config{users}{admin_name}/) {
+  my $password = $user->{password};
+  if ($username eq $config{users}{admin_name}) {
     $logger->info("User admin. SKIP.");
     next;
   }
@@ -165,12 +172,12 @@ foreach my $user (@{$users}) {
   # if check_option is weekly|monthly and (current_time-last_check_time) < week|month
   #   copy last user report to new global report
   #   skip check
-  if (defined $last_user_report and $check_interval) { # check_interval == 0 -> Automatic so do the check
+  if (defined $last_user_report and $check_interval) { # check_interval == 0 -> 'Automatic' so do the check
     $logger->info(sprintf "%s opts for %s checks. Checking report time interval", $username, $check_interval==1?'weekly':'monthly');
     my $current_time = time;
     my $last_check_time = $last_user_report->{end_time};
     defined $last_check_time or 
-      $logger->logdie("Undefined last_check_time in last report for user $username");
+      $logger->logdie("Undefined last check time in last report for user $username");
     # check_interval == 1 -> week, 2 -> month
     my $time_interval =  $check_interval==1?604800:2592000;
 
@@ -186,9 +193,9 @@ foreach my $user (@{$users}) {
   my @trackdbs;
   try {
     map { push @trackdbs, Registry::TrackHub::TrackDB->new($_->{_id}) }
-      @{$es->search_trackhubs(size => 1000000, query => { term => { owner => $username } })->{hits}{hits}};
+      @{$es->get_trackdbs(query => { term => { owner => $username } })};
   } catch {
-    $logger->logdie($_);
+    $logger->logdie("Unable to get trackDBs for user $username:\n$_");
   };
   
   $logger->info("User has no trackdbs. SKIP") and next unless scalar @trackdbs;
@@ -202,103 +209,112 @@ foreach my $user (@{$users}) {
   #   format message along the way
   my ($message_body_update, $message_body_problem);
   my $trackdb_update = 0;
+  my %updated_trackhubs; # record hubs which have been updated
 
   foreach my $trackdb (@trackdbs) {
-    $logger->info(sprintf "Checking trackDb [%d]", $trackdb->id);
-    my $source = $trackdb->source;
-    if ($source) {
-      $logger->info(sprintf "trackDb is sourced from UCSC hub.");
-      $logger->info("checking remote updates at %s", );
+    #
+    # WARNING: 
+    #   - Can check if trackDB has changed but cannot resubmit as I should know the mapping 
+    #     from assembly name to INSDC accession, eventually. This is information which provided 
+    #     by the submitter.
+    #   - Also, we should be able to detect a case where the entire structure of the hub might have
+    #     changed, with some trackDBs which do not exist any more.
+    #
+    # my $source = $trackdb->source;
+    # if ($source) {
+    #   $logger->info(sprintf "trackDb is sourced from UCSC hub.");
+    #   $logger->info("checking remote updates at %s", );
       
-      defined $source->{url} and defined $source->{checksum}
-	or $logger->logdie("Document source doesn't have url/checksum attributes");
+    #   defined $source->{url} and defined $source->{checksum}
+    # 	or $logger->logdie("Document source doesn't have url/checksum attributes");
 
-      try {
-	# checksum is not enforced in schema, but it must exist once
-	# the remote hub JSON has been submitted
+    #   try {
+    # 	# checksum is not enforced in schema, but it must exist once
+    # 	# the remote hub JSON has been submitted
+    # 	my $checksum = $trackdb->compute_checksum;
+    # 	if ($checksum and $checksum ne $source->{checksum}) {
+    # 	  $logger->info("Remote source has changed, updating.");
 
-	my $checksum = $trackdb->compute_checksum;
-       
-	if ($checksum and $checksum ne $source->{checksum}) {
-	  $logger->info("Remote source has changed, updating.");
+    # 	  $trackdb_update = 1;
+    # 	  $message_body_update .= sprintf "Detected trackDB [%s] source URL update, resubmitting to Registry: ";
 
-	  $trackdb_update = 1;
-	  $message_body_update .= sprintf "Detected trackDB [%s] source URL update: ";
+    # 	  $logger->info(sprintf "Translating trackDb source to JSON version %s.", $trackdb->version);
+    # 	  my $translator = Registry::TrackHub::Translator->new(version => $trackdb->version);
+    # 	  my $updated_json_doc = $translator->translate($trackdb->hub->{url}, $trackdb->assembly->{synonyms} || 'unknown')->[0];
 
-	  $logger->info(sprintf "Translating trackDb source to JSON version %s.", $trackdb->version);
-	  my $translator = Registry::TrackHub::Translator->new(version => $trackdb->version);
-	  my $updated_json_doc = $translator->translate($trackdb->hub->{url}, $trackdb->assembly->{synonyms} || 'unknown')->[0];
+    # 	  $logger->info("Validating translated document.");
+    # 	  my $validator = 
+    # 	    Registry::TrackHub::Validator->new(schema => Registry->config()->{TrackHub}{schema}{$trackdb->version});
+    # 	  my ($fh, $filename) = tempfile( DIR => '.', SUFFIX => '.json', UNLINK => 1); print $fh $updated_json_doc; close $fh;
+    # 	  $validator->validate($filename);
 
-	  $logger->info("Validating translated document.");
-	  my $validator = 
-	    Registry::TrackHub::Validator->new(schema => Registry->config()->{TrackHub}{schema}{$trackdb->version});
-	  my ($fh, $filename) = tempfile( DIR => '.', SUFFIX => '.json', UNLINK => 1); print $fh $updated_json_doc; close $fh;
-	  $validator->validate($filename);
+    # 	  my $updated_doc = from_json($updated_json_doc);
 
-	  my $updated_doc = from_json($updated_json_doc);
-
-	  # TODO? Prevent submission of duplicate content
-	  # i.e. trackdb with same hub/assembly
-	  # it shouldn't be the case
+    # 	  # TODO? Prevent submission of duplicate content
+    # 	  # i.e. trackdb with same hub/assembly
+    # 	  # it shouldn't be the case
 	  
-	  # set the owner/created/updated/status/source attributes
-	  $updated_doc->{owner} = $username;
-	  $updated_doc->{created} = $trackdb->created;
-	  $updated_doc->{updated} = time;
-	  $updated_doc->{status}{message} = 'Unknown';
-	  $updated_doc->{source}{checksum} = $checksum;
+    # 	  # set the owner/created/updated/status/source attributes
+    # 	  $updated_doc->{owner} = $username;
+    # 	  $updated_doc->{created} = $trackdb->created;
+    # 	  $updated_doc->{updated} = time;
+    # 	  $updated_doc->{status}{message} = 'Unknown';
+    # 	  $updated_doc->{source}{checksum} = $checksum;
 
-	  $logger->info("Writing document store with updates.");
-	  $es->index(index   => $config->{trackhub}{index},
-		     type    => $config->{trackhub}{type},
-		     id      => $trackdb->id,
-		     body    => $updated_doc);
-	  $es->indices->refresh(index => $config->{trackhub}{index});
+    # 	  $logger->info("Writing document store with updates.");
+    # 	  $es->index(index   => $config->{trackhub}{index},
+    # 		     type    => $config->{trackhub}{type},
+    # 		     id      => $trackdb->id,
+    # 		     body    => $updated_doc);
+    # 	  $es->indices->refresh(index => $config->{trackhub}{index});
 
-	  # re-instantiate the trackdb since the document has changed
-	  $trackdb = Registry::TrackHub::TrackDB->new($trackdb->id);
+    # 	  # re-instantiate the trackdb since the document has changed
+    # 	  $trackdb = Registry::TrackHub::TrackDB->new($trackdb->id);
 
-	  $message_body_update .= "SUCCESS.\n\n";
-	}
-      } catch {
-	$logger->warn($_);
-	$message_body_update .= "ERROR.\n$@\n";
+    # 	  $message_body_update .= "SUCCESS.\n\n";
+    # 	}
+    #   } catch {
+    # 	$logger->warn($_);
+    # 	$message_body_update .= "ERROR.\n$@\n";
 
-	# we simply skip update and the status check
-	next;
-      };
-    }
-    
-    $logger->info("Checking trackDb track status.");
+    # 	# we simply skip update and the status check
+    # 	next;
+    #   };
+    # }
+
+    my ($id, $hub, $assembly) = ($trackdb->id, $trackdb->hub->{name}, $trackdb->assembly->{name});
+    $logger->info(sprintf "Checking trackDb [%s] (hub: %s, assembly: %s)", $id, $hub, $assembly);    
     my $status;
     try {
       $status = $trackdb->update_status();
     } catch {
       $logger->warn($_);
 
-      $message_body_problem .= "Problem updating status for trackDB [%s]\n$@\n\n", $trackdb->id;
+      $message_body_problem .= 
+	sprintf "Problem updating status for trackDB [%s] (hub: %s, assembly: %s)\n$@\n\n", 
+	  $id, $hub, $assembly;
       next;
     };
    
     # if problem add trackdb to user report
     if ($status->{tracks}{with_data}{total_ko}) {
       $logger->info("There are faulty tracks, updating report.");
-      $current_user_report->{ko}{$trackdb->id} =
+      $current_user_report->{ko}{$id} =
 	$status->{tracks}{with_data}{ko};
 
-      $message_body_problem .= sprintf "trackDB [%d] (%s, %s)\n", $trackdb->id, $trackdb->hub->{name}, $trackdb->assembly->{accession};
-      foreach my $track (keys %{$current_user_report->{ko}{$trackdb->id}}) {
-	$message_body_problem .= sprintf "\t%s\t%s\t%s\n", $track, $current_user_report->{ko}{$trackdb->id}{$track}[0], $current_user_report->{ko}{$trackdb->id}{$track}[1];
+      $message_body_problem .= sprintf "trackDB [%d] (hub: %s, assembly: %s)\n", $id, $hub, $assembly;
+      foreach my $track (keys %{$current_user_report->{ko}{$id}}) {
+	$message_body_problem .= sprintf "\t%s\t%s\t%s\n", $track, $current_user_report->{ko}{$id}{$track}[0], $current_user_report->{ko}{$id}{$track}[1];
       }
       $message_body_problem .= "\n\n";
     } else {
-      push @{$current_user_report->{ok}}, $trackdb->id;
+      push @{$current_user_report->{ok}}, $id;
     }
   }
   $current_user_report->{end_time} = time;
   $logger->info("Finished with $username trackDbs.");
-  $logger->info("Checking alert report status.");
-  
+
+  $logger->info("Checking alert report status.");  
   if ($trackdb_update or scalar keys %{$current_user_report->{ko}}) { # user trackdbs have problems
     # format complete message body
     my $message_body = "This alert report has been automatically generated during the last update of the TrackHub Registry.\n\n";
@@ -309,7 +325,7 @@ foreach my $user (@{$users}) {
       Email::MIME->create(
 			  header_str => 
 			  [
-			   From    => 'admin@trackhubregistry.org',
+			   From    => 'avullo@ebi.ac.uk',
 			   To      => $user->{email},
 			   Subject => 'Alert report from TrackHub Registry',
 			  ],
@@ -321,30 +337,27 @@ foreach my $user (@{$users}) {
 			  body_str => $message_body,
 			 );
 
-    # check whether we have to send an alert 
-    # to the user and send it, eventually
+    # check whether we have to send an alert to the user and send it, eventually
     try {
-      if ($last_user_report) {
-	if ($continuous_alert) {
-	  # user wants to be continuously alerted: send message anyway 
-	  $logger->info("$username opts for continuous alerts. Sending.");
-	  sendmail($message);
-	} else {
-	  # user doesn't want to be bothered more than once with the same problems
-	  # send alert only if current report != last report
-	  $logger->info("$username does not opt for continuous alerts. Checking differences with last report.");
-	  if ($last_user_report->{ko}) {
-	    my @last_report_ko_trackdbs = keys %{$last_user_report->{ko}};
-	    my @current_report_ko_trackdbs = keys %{$current_user_report->{ko}};
-	    my ($union, $isect, $diff) = 
-	      union_intersection_difference(\@current_report_ko_trackdbs, \@last_report_ko_trackdbs);
+      if ($continuous_alert) {
+	# user wants to be continuously alerted: send message anyway 
+	$logger->info("$username opts for continuous alerts. Sending.");
+	sendmail($message);
+      } elsif ($last_user_report) {
+	# user doesn't want to be bothered more than once with the same problems
+	# send alert only if current report != last report
+	$logger->info("$username does not opt for continuous alerts. Checking differences with last report.");
+	if ($last_user_report->{ko}) {
+	  my @last_report_ko_trackdbs = keys %{$last_user_report->{ko}};
+	  my @current_report_ko_trackdbs = keys %{$current_user_report->{ko}};
+	  my ($union, $isect, $diff) = 
+	    union_intersection_difference(\@current_report_ko_trackdbs, \@last_report_ko_trackdbs);
 	  
-	    if (scalar @{$diff}) {
-	      $logger->info("Detected difference. Sending alert report anyway.");
-	      sendmail($message);
-	    } else {
-	      $logger->info("No difference. Not sending the alert report.");
-	    }
+	  if (scalar @{$diff}) {
+	    $logger->info("Detected difference. Sending alert report anyway.");
+	    sendmail($message);
+	  } else {
+	    $logger->info("No difference. Not sending the alert report.");
 	  }
 	}
       } else {
@@ -384,7 +397,6 @@ if ($current_report) {
 }
 
 $logger->info("Sending alert report to admin.");
-
 my $message = 
   Email::MIME->create(
 		      header_str => 
@@ -408,6 +420,24 @@ try {
 };
 
 $logger->info("DONE.");
+
+# sub submit_trackhub {
+#   my ($user, $pass, $url) = @_;
+#   defined $user or die "Undefined user";
+#   defined $pass or die "Undefined password for user $user";
+#   defined $url or die "Undefined hub URL";
+
+#   my $ua = LWP::UserAgent->new;
+#   my $request = GET("$server/api/login");
+#   $request->headers->authorization_basic($user, $pass);
+#   my $response = $ua->request($request);
+#   my $token = from_json($response->content)->{auth_token}
+#     if $response->is_success;
+#   defined $token or die "User unable to login";
+
+#   $request = POST("$server/api/trackhub"); 
+# }
+
 
 __END__
 

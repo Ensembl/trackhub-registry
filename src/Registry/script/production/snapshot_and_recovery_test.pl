62d5dde8217212279d6e40405d53d999dc5b3ba7
Alessandro Vullo <avullo@ebi.ac.uk>
2016-02-08 14:10:45 +0000

    - Send alert message and log die in case any problem is detected - Use LWP requests for any direct query to the ES REST endpoints - Parse response from recovery status API directly into loop, calling   the function somehow compromise the data passed in

--- src/Registry/script/production/snapshot_and_recovery_test.pl
+++ src/Registry/script/production/snapshot_and_recovery_test.pl
@@ -14,6 +14,8 @@ use JSON;
 use HTTP::Tiny;
 use LWP::UserAgent;
 use HTTP::Request::Common qw/GET POST/;
+use Email::MIME;
+use Email::Sender::Simple qw(sendmail);
 use Search::Elasticsearch;
 
 # default option values
@@ -70,9 +72,6 @@ my $snapshot_name = sprintf "snapshot_%s", $date;
 # we backup only relevant indices 
 my $indices = join(',', qw/trackhubs users reports/);
 $logger->info("Creating snapshot ${snapshot_name} of indices $indices");
-# TODO
-# - monitor progress
-# - email in case of problem
 eval {
   $es->snapshot->create(repository  => $config{repository}{name},
 			snapshot    => $snapshot_name,
@@ -80,7 +79,12 @@ eval {
 					indices => $indices
 				       });
 }; # catch {
-$logger->logdie("Couldn't take snapshot ${snapshot_name}: $@") if $@;
+if ($@) {
+  my $message = "Couldn't take snapshot ${snapshot_name}: $@";
+  send_alert_message($message);
+  $logger->logdie($message);
+}
+
 #};
 
 # monitor snapshot status, cannot proceed to restore
@@ -91,7 +95,11 @@ do {
 					   snapshot    => $snapshot_name)->{snapshots}[0]{state};
 } while ($snapshot_status eq 'IN_PROGRESS' or $snapshot_status eq 'STARTED');
 
-$logger->logdie("Taking Snapshot $snapshot_name failed") unless $snapshot_status eq 'SUCCESS';
+unless ($snapshot_status eq 'SUCCESS') {
+  my $message = "Taking Snapshot ${snapshot_name failed}";
+  send_alert_message($message);
+  $logger->logdie($message);
+}
 
 #######################################################
 #
@@ -101,7 +109,11 @@ $es = connect_to_es_cluster($config{cluster_staging});
 
 $logger->info("Closing indices on staging cluster");
 eval {$es->indices->close(index => [ split /,/, $indices ]); };
-$logger->logdie("Failed closing indices on staging server: $@") if $@;
+if ($@) {
+  my $message = "Failed closing indices on staging server: $@";
+  send_alert_message($message);
+  $logger->logdie($message);
+}
 
 $logger->info("Restoring from snapshot ${snapshot_name}");
 # TODO
@@ -118,24 +130,95 @@ $logger->info("Restoring from snapshot ${snapshot_name}");
 # };# catch {
 # $logger->logdie("Failed restoration from snapshot ${snapshot_name}: $@") if $@;
 #};
-my $response = HTTP::Tiny->new()->request('POST', 
-					  sprintf "http://%s/_snapshot/backup/%s/_restore", $config{cluster_staging}{nodes}, $snapshot_name, 
-					  { content => { indices => $indices } });
+my $ua = LWP::UserAgent->new;
+my $request = 
+  POST(sprintf "http://%s/_snapshot/backup/%s/_restore", $config{cluster_staging}{nodes}, $snapshot_name,
+       'Content-type' => 'application/json',
+       'Content'      => to_json({ indices => $indices }));
+my $response = $ua->request($request);
+if ($response->code != 200) {
+  my $message = sprintf "Couldn't restore from snapshot ${snapshot_name}:\n%s", from_json($response->content);
+  send_alert_message($message);
+  $logger->logdie($message);
+}
+# my $response = 
+#   HTTP::Tiny->new()->request('POST', 
+# 			     sprintf "http://%s/_snapshot/backup/%s/_restore", $config{cluster_staging}{nodes}, $snapshot_name, 
+# 			     { content => { indices => $indices } });
 
 # monitor restoring process
 eval {
   my ($response, $restore_status);
+  my $complete = 1;
   do {
     # $response = HTTP::Tiny->new()->request('GET', sprintf "http://%s/_cat/recovery?v", $config{cluster_staging}{nodes});
-    $response = HTTP::Tiny->new()->request('GET', sprintf "http://%s/_recovery?pretty&human", $config{cluster_staging}{nodes});
-    print Dumper $response; exit;
+    # $response = HTTP::Tiny->new()->request('GET', sprintf "http://%s/_recovery?pretty&human", $config{cluster_staging}{nodes});
+    # print Dumper $response; exit;
 
-    # my $ua = LWP::UserAgent->new;
-    # my $request = GET(sprintf "http://%s/_recovery?pretty&human", $config{cluster_staging}{nodes});
-    # $response = $ua->request($request);
-  } while (not restore_complete($response->content));
+    my $request = GET(sprintf "http://%s/_recovery?pretty&human", $config{cluster_staging}{nodes});
+    $response = $ua->request($request);
+    die "Couldn't query the recovery endpoint for status"
+      unless $response->code != 200;
+
+    my $info;
+
+    #
+    # this is to parse the response to the cat API call
+    #
+    # open my $FH, '<', \$response or die "Cannot read response: $!\n";
+    # <$FH>; # first line is header
+    # while (my $line = <$FH>) {
+    #   chomp ($line);
+    #   next if $line =~ /^\s/;
+    #   my ($index, $shard, $time, $type, $stage, $source_host, $target_host, $repository, $snapshot, $files, $files_percent, $bytes, $bytes_percent, $total_files, $total_bytes, $translog, $translog_percent, $total_translog) =
+    #     split /\s+/, $line;
+    #   $info->{$index}{$shard} = $stage;
+    #   print "$index\t$shard\t$stage\n";
+    # }
+    # close $FH;
+
+    # foreach my $index (keys %{$info}) {
+    #   foreach my $shard (keys %{$info->{$index}}) {
+    #     # we die if we get unexpected stage so that we
+    #     # can interrupt the monitoring process
+    #     my $stage = $info->{$index}{$shard};
+    #     die "Something unexpected happened during recovery, stage: $stage"
+    # 	unless ($stage eq 'done' or $stage eq 'index' or $stage eq 'init');
+    #     return 0 if $info->{$index}{$shard} ne 'done';
+    #   }
+    # }
+
+    my $content = eval { from_json($response->content); };
+    die "Could't parse recovery endpoint status response: $@" if $@;
+    
+    print Dumper $content; 
+
+  OUTER:
+    foreach my $index (keys %{$content}) {
+      foreach my $shard (@{$content->{shards}}) {
+	# we die if we get unexpected stage so that we
+	# can interrupt the monitoring process
+	my $stage = $shard->{stage};
+	print "$index\t", $shard->{id}, "\t$stage\n"; 
+	die "Something unexpected happened during recovery, stage: $stage"
+	  unless ($stage eq 'DONE' or $stage eq 'INDEX' or $stage eq 'INIT');
+	if ($stage ne 'DONE') {
+	  $complete = 0;
+	  last OUTER;
+	}
+
+      }
+    }
+    
+    print "\n$complete -- "; <STDIN>;
+  } while (not $complete); # not restore_complete($response->content));
 };
-$logger->logdie($@) if $@;
+if ($@) {
+  my $message = "Something went wrong monitoring recovering from snapshot ${snapshot_name}:\n$@";
+  send_alert_message($message);
+  $logger->logdie($message);
+}
+
 
 # $logger->info("Reopening indices");
 # $es->indices->open(index => [ split /,/, $indices ]);
@@ -173,6 +256,8 @@ sub restore_complete {
   # }
 
   my $content = from_json($response);
+  return 0 unless $content;
+
   foreach my $index (keys %{$content}) {
     foreach my $shard (@{$content->{shards}}) {
       # we die if we get unexpected stage so that we
@@ -206,6 +291,30 @@ sub connect_to_es_cluster {
   $logger->info("Instantiating ES client");
   return Search::Elasticsearch->new(cxn_pool => 'Sniff',
 				    nodes => $nodes);
+}
+
+sub send_alert_message {
+  my $body = shift;
+
+  my $localtime = localtime;
+  my $message = 
+    Email::MIME->create(
+			header_str => 
+			[
+			 From    => 'avullo@ebi.ac.uk',
+			 To      => 'avullo@ebi.ac.uk',
+			 Subject => sprintf("Alert report from TrackHub Registry: %s", $localtime),
+			],
+			attributes => 
+			{
+			 encoding => 'quoted-printable',
+			 charset  => 'ISO-8859-1',
+			},
+			body_str => $body,
+		       );
+  
+  $logger->info("Sending alert report to admin");
+  sendmail($message);  
 }
 
 __END__

4b8cd8298ddd7ee686f2efcf2f0f4775f0154835
Alessandro Vullo <avullo@ebi.ac.uk>
2016-07-01 16:20:51 +0100

    [ENSCORESW-1828]. Should expand correctly now.

--- src/Registry/script/production/test_login.sh
+++ src/Registry/script/production/test_login.sh
@@ -3,9 +3,9 @@
 cd ${HOME}/src/trackhub-registry/src/Registry/script/production
 user=$1
 pass=$2
-
+date=`date`
 perl test_login.pl $user $pass || {
-    printf '%s - Logging does not work. Restarting server...' `date` >> logs/test_loging.log
+    printf '%s - Logging does not work. Restarting server...' "$date" >> logs/test_loging.log
     # stop_server.sh
     # start_server.sh
     # exit 1

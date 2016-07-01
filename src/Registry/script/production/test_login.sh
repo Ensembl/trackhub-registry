19cdef77a7dbc9ec79e2953f03b34bad3e9c5e38
Alessandro Vullo <avullo@ebi.ac.uk>
2016-07-01 16:19:01 +0100

    [ENSCORESW-1828]. Include date in log.

--- src/Registry/script/production/test_login.sh
+++ src/Registry/script/production/test_login.sh
@@ -5,7 +5,7 @@ user=$1
 pass=$2
 
 perl test_login.pl $user $pass || {
-    printf 'Logging does not work\nRestarting server...' >> logs/test_loging.log
+    printf '%s - Logging does not work. Restarting server...' `date` >> logs/test_loging.log
     # stop_server.sh
     # start_server.sh
     # exit 1

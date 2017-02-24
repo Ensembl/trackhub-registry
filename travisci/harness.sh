f18631affcf2528ce0c8a23bace67ca0bd4cfbbe
Alessandro Vullo <avullo@ebi.ac.uk>
2017-02-24 10:13:53 +0000

    [ENSCORESW-2192]. Some exploratory tests.

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -6,15 +6,14 @@ echo $ENSDIR
 export PERL5LIB=$ENSDIR/bioperl-live:$ENSDIR/ensembl/modules:$ENSDIR/ensemblgenomes-api/modules:$PWD/src/Registry/lib
 export PYTHONPATH=/usr/local/lib/python2.7/dist-packages/:$PYTHONPATH
 
-echo "import jsonschema" > test.py
-echo "print 'Import succeeded'" >> test.py
-echo "Running python test"
-python test.py
-echo "done"
-
 echo "Running test suite"
 export TEST_POD=1
 export CATALYST_DEBUG=0
+
+cd src/Registry
+rm -rf blib
+t/auth/script/setup.pl # initialisation for testing the authentication module
+prove t/track_hub/validator.t
 
 rt=$?
 if [ $rt -eq 0 ]; then

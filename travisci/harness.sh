16d093fd7c4bc917b1a2dcc365340e9e1d9e32ee
Kieron Taylor <ktaylor@ebi.ac.uk>
2019-03-05 14:14:25 +0000

    Now we are using a custom install, we no longer need to try to force the config location

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -10,8 +10,6 @@ echo "Running test suite"
 export TEST_POD=1
 export CATALYST_DEBUG=0
 export ES_HOME=/usr/share/elasticsearch
-mkdir $HOME/es_conf
-export ES_PATH_CONF=$HOME/es_conf
 
 cd src/Registry
 rm -rf blib
